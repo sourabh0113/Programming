652b7b27690121d64bffc40e205fcfcbb2b1962a
rohit chouhan <sourabhdalal28@gmail.com>
2024-07-01 18:00:02 +0530

    program for simple calculation

---
+++ out.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter the number";
+    cin>>a>>b;
+    cout<<"\naddition"<< a+b;
+    cout<<"\nsubtraction"<< a-b;
+    return 0;
+}
