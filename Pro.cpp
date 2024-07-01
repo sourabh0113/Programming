1c2db72e58b02c184af1c800534332d6a0f56191
rohit chouhan <sourabhdalal28@gmail.com>
2024-07-01 17:18:59 +0530

    simple program

---
+++ Pro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+cout<<"Enter the 2 number\n";
+cin>>a>>b;
+cout<<"\n addition "<<a+b;
+cout<<"\n subtraction "<<a-b;
+cout<<"\n multiplication "<<a*b;
+cout<<"\n division "<<a/b;
+
+    return 0;
+}
