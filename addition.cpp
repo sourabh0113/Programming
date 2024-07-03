b3cdd7e017d040ab0c161833e154a1a156864a19
rohit chouhan <sourabhdalal28@gmail.com>
2024-07-03 17:26:24 +0530

    addition of two number

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+
+    cout<<"addition of two number";
+    cin>>a>>b;
+
+    cout<<"\naddition"<<a+b;
+    return 0;
+
+}
---
+++ tut.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int n1,n2;
+cout<<"which is the biggest value";
+
+cin>>n1>>n2;
+
+if(n1>n2){
+    cout<<"\n"<<n1;
+}
+else{
+    cout<<"\n"<<n2;
+}
+}
