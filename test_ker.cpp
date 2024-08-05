8e3974128a66e007ac63a0e255e446e586581d08
your github sourabh0113 <your github sourabhdalal28@gmail.com>
2024-08-05 13:31:24 +0530

    test1q2

---
+++ test_ker.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,d,c,b;
+
+    cout<<"enter the values"<<endl;
+    cin>>a>>b>>c>>d;
+
+    cout<<"result"<<((a*d)/c+b);
+
+    return 0;
+
+}
