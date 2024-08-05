a587d7ee481a604e389eebb6d7c91277c4f3f490
your github sourabh0113 <your github sourabhdalal28@gmail.com>
2024-08-05 13:32:42 +0530

    test1q3

---
+++ test_ker2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool aadhar = true;
+    bool pancard = true;
+
+    if(aadhar == true && pancard == true){
+        cout<<"you can open an account"<<endl;
+    }
+    else{
+        cout<<"you can't open an account"<<endl;
+    
+    }
+    return 0;
+    
+}
+
