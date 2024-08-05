c914f2aae911a025b7d535c81d00c16efd44ffca
your github sourabh0113 <your github sourabhdalal28@gmail.com>
2024-08-05 13:34:16 +0530

    test1q3

---
+++ test_ker3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+
+cout<<"enter the vaule";
+cin>>num;
+
+    if (num>=20000){
+        cout<<"person can purchase mobile"<<endl;
+
+    }
+    else{
+        cout<<"person cannot purchase mobile"<<endl;
+    }
+    return 0;
+}
