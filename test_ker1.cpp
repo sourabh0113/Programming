336654d0602983e559b5a05ce3610ebd1ac53ae1
your github sourabh0113 <your github sourabhdalal28@gmail.com>
2024-08-05 13:33:20 +0530

    test1q1

---
+++ test_ker1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+   int num;
+     cout<<"enter number"<<endl;
+    cin>>num;
+
+if(num%2==0){
+    cout<<"even numnber"<<endl;
+
+}
+else{
+    cout<<"odd number"<<endl;
+}
+
+
+    return 0 ;
+}
+
